9ac7e9da848f856052fbd93d2b72150a914245c1
Sahto <liprerinho@Outlook.com>
2023-05-29 17:32:22 -0300

    Initial commit

---
+++ costs/src/App.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Routes, Route,} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Project from './components/pages/Project';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Container customClass="min-height">
+      <Routes>
+          <Route path="/" exact element={<Home/>}> </Route>
+          <Route path="/company"  element={<Company/>}> </Route>
+          <Route path="/projects"  element={<Projects/>}> </Route>
+          <Route path="/contact"  element={<Contact/>}> </Route>
+          <Route path="/newproject"  element={<NewProject/>}> </Route>
+          <Route path="/project/:id"  element={<Project/>}> </Route>
+      </Routes>
+      </Container>
+      <Footer/>
+    </Router>
+
+  )
+}
+
+export default App;
---
+++ costs/src/components/pages/Project.js
@@ -0,0 +1,63 @@
+import style from './ProjectE.module.css'
+import { useParams } from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import Loading from '../layout/Loading'
+import Container from '../layout/Loading'
+
+function Project() {
+
+    const { id } = useParams()
+    
+    const[ project, setProject ] = useState ([])
+    const [showProjectForm, setShowProjectForm] = useState(false)
+
+    useEffect (() => {
+        setTimeout(() =>{
+            fetch(`http://localhost:5000/projects/${id}`,{
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data)
+            })
+            .catch((err) => console.log)
+        },300)
+    }, [id])
+
+        function toggleProjectForm() {
+            setShowProjectForm(!showProjectForm)
+        }
+ 
+
+    return(
+        <>
+        {project.name ? (
+            <div>
+                <Container customClass="column">
+                    <div>
+                        <h1>Projeto: {project.name}</h1>
+                        <button onClick={toggleProjectForm}> 
+                            {!showProjectForm? 'Editar Projeto' : "Fechar"}
+                            </button>
+                            {!showProjectForm ?(
+                                <div>
+                                    <p>project Form</p>
+                                </div>    
+                            ): (
+                                <div>
+                                    <p>detalhes do projeto</p>
+                                </div>  
+                            )}
+                    </div>
+                </Container>
+            </div>     
+            ) : (
+            <Loading />
+        )}
+        </>
+    )
+}
+export default Project
